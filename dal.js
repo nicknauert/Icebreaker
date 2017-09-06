b9c3eb23e3e58818391b86663cf0c848ecfe63b3
Alonya Lafayette <alonyalafayette1@gmail.com>
2017-09-06 16:30:38 -0400

    test

--- dal.js
+++ dal.js
@@ -1,5 +1,25 @@
 const chalk = require('chalk');
 const {} = require('./models/models.js');
+mongoose.Promise = require('bluebird')
+mongoose.connect('mongodb://localhost:27017/playerSchema', {
+  useMongoClient: true
+});
+
+function getCategory(){
+  return
+}
+
+function getDiff(){
+  return
+}
+
+function getTrivia(){
+  return
+}
 
 
-module.exports = {}
+module.exports = {
+  getCat,
+  getDif,
+  getTrivia,
+}
