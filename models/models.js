fabad163704ae267985289834c27559f1e47c134
Chad Crabtree <chad.rc.crabtree@gmail.com>
2017-09-06 17:09:04 -0400

    WIP condensed schemas

--- models/models.js
+++ models/models.js
@@ -2,62 +2,7 @@ const mongoose = require('mongoose');
 const schema = mongoose.schema;
 
 
-const musicEasySchema = new mongoose.Schema({
-  {
-    Category: String,
-    Type: String,
-    Difficulty: String,
-    Question: String,
-    Correct_Answer: String,
-    Incorrect_Answer: [{type: String}],
-  },
-})
-
-const musicHardSchema = new mongoose.Schema({
-  {
-    Category: String,
-    Type: String,
-    Difficulty: String,
-    Question: String,
-    Correct_Answer: String,
-    Incorrect_Answer: [{type: String}],
-  },
-})
-
-const sportsEasySchema = new mongoose.Schema({
-  {
-    Category: String,
-    Type: String,
-    Difficulty: String,
-    Question: String,
-    Correct_Answer: String,
-    Incorrect_Answer: [{type: String}],
-  },
-})
-
-const sportsHardSchema = new mongoose.Schema({
-  {
-    Category: String,
-    Type: String,
-    Difficulty: String,
-    Question: String,
-    Correct_Answer: String,
-    Incorrect_Answer: [{type: String}],
-  },
-})
-
-const computersEasySchema = new mongoose.Schema({
- {
-   Category: String,
-   Type: String,
-   Difficulty: String,
-   Question: String,
-   Correct_Answer: String,
-   Incorrect_Answer: [{type: String}],
-  },
-})
-
-const computersHardSchema = new mongoose.Schema({
+const questionsSchema = new mongoose.Schema({
   {
     Category: String,
     Type: String,
@@ -69,21 +14,9 @@ const computersHardSchema = new mongoose.Schema({
 })
 
 
-
-const musicEasySchema = mongoose.model('musicEasy', musicEasySchema);
-const musicHardSchema = mongoose.model('musicHard', musicHardSchema);
-const sportsEasySchema = mongoose.model('sportsEasy', sportsEasySchema);
-const sportsHardSchema = mongoose.model('sportsHard', sportsHardSchema);
-const computersEasySchema = mongoose.model('computersEasy', computersEasySchema);
-const computersHardSchema = mongoose.model('computersHard', computersHardSchema);
-
+const questionsSchema = mongoose.model('questions', questionsSchema);
 
 
 module.exports = {
-  musicEasy,
-  musicHard,
-  sportsEasy,
-  sportsHard,
-  computersEasy,
-  computersHard
+  questions
 }
