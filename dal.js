5f8aa52fda7c3c3e9290e345e15704721491af84
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-09-06 17:22:01 -0400

    Hackathon: added mongoose on dal with db route /triviadb.

--- dal.js
+++ dal.js
@@ -1,7 +1,12 @@
+const mongoose = require('mongoose')
+mongoose.Promise = require('bluebird')
+mongoose.connect('mongodb://localhost:27017/triviadb', {
+  useMongoClient: true
+})
 const chalk = require('chalk');
-const {} = require('./models/models.js');
+const { questions } = require('./models/models.js');
 
-function 
+function
 
 
 module.exports = {}
