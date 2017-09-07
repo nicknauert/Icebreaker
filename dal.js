3f63a5047cfe66ecd692bd9eee051c129c9788f8
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-09-07 15:30:38 -0400

    Completed working countdown clock

--- dal.js
+++ dal.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose')
-mongoose.Promise = require('bluebird');
+mongoose.Promise = require('bluebird')
 const chalk = require('chalk');
 const { Question } = require('./models/models.js');
+const {Trivia} = require('./models/models.js');
 
 
+<<<<<<< HEAD
 
 function getQuestion(cat, diff){
   return Question.find({ category: cat, difficulty: diff }).then((quesArr)=>{
 
+=======
+function getQuestion(cat, diff){
+  return Question.find({ category: cat, difficulty: diff }).then((quesArr)=>{
+>>>>>>> 4307236f1cb6e24ee979abf60e5f1a47468889eb
     let number = Math.floor(Math.random() * quesArr.length)
     return quesArr[number]
   })
--- models/models.js
+++ models/models.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose');
 const schema = mongoose.schema;
 
 
+
 const QuestionSchema = new mongoose.Schema({
     category: String,
     type: String,
     difficulty: String,
     question: String,
     correct_answer: String,
+<<<<<<< HEAD
     incorrect_answers: [{type: String}]
+=======
+    incorrect_answers: [{type: String}],
+>>>>>>> 4307236f1cb6e24ee979abf60e5f1a47468889eb
 })
 
 
