36277ae13a33542f97e3be4975406f229c4723de
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-09-08 20:13:44 -0400

    Timer working. Music added and working with the timer. Working on fading out sound or using the back end of the jeopardy clip so the ending isn' so abrupt. Timer is on page with basic inline styling just for dev purposes.

--- public/main.js
+++ public/main.js
@@ -1,21 +1,30 @@
-document.getElementById("MyElement").classList.add('MyClass');
+// document.getElementById("MyElement").classList.add('MyClass');
+//
+// document.getElementById("MyElement").classList.remove('MyClass');
+//
+//
+// function showAnswers(){
+//
+//   document.querySelector("button").classList.remove('baseColor');
+//   document.querySelector(".answer").classList.add('correctColor');
+//   document.querySelector(".wrongAnswer").classList.add('wrongColor');
+//
+// }
+// //========== event listener ==========
+// const main = document.querySelector('.gamePage')
+//
+// main.addEventListener('load', function(event) {
+//   event.preventDefault()
+//   console.log('load event listener woked');
+// })
 
-document.getElementById("MyElement").classList.remove('MyClass');
-
-
-function showAnswers(){
-
-  document.querySelector("button").classList.remove('baseColor');
-  document.querySelector(".answer").classList.add('correctColor');
-  document.querySelector(".wrongAnswer").classList.add('wrongColor');
-
-}
 
 
 // ========== timer code ==========
-const fifteenSeconds = .16667;
+const tenSeconds = .16667;
 const currentTime = Date.parse(new Date());
-const totalTime = new Date(currentTime + fifteenSeconds*60*1000);
+const totalTime = new Date(currentTime + tenSeconds*60*1000);
+const audio = document.querySelector('audio')
 
 function createTimer(endtime){
   const t = Date.parse(endtime) - Date.parse(new Date());
@@ -27,7 +36,7 @@ function createTimer(endtime){
 
 function startTimer(id, endtime){
   const timer = document.getElementById(id);
-  const timeinterval = setInterval(function(){
+  const timeinterval = setInterval(()=> {
     const t = createTimer(endtime);
     timer.innerHTML = t.seconds;
     if(t.total <= 0){
@@ -35,5 +44,12 @@ function startTimer(id, endtime){
     }
   },1000);
 }
+
+//code base from stack overflow on audio timer
+audio.addEventListener('canplaythrough', function() {
+  setTimeout(function() {
+    audio.pause()
+  }, 10000);
+}, false)
 
 startTimer('timer', totalTime)
